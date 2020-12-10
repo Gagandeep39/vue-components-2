6f68e98d20a08238f8edcdeb40f03e6acd625e50
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-10 13:04:07 +0000

    Added dummy comonents

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import ActiveElement from './components/ActiveElement.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
+
+createApp(App)
+  .component('active-element', ActiveElement)
+  .component('knowledge-base', KnowledgeBase)
+  .component('knowledge-element', KnowledgeElement)
+  .component('knowledge-grid', KnowledgeGrid)
+  .mount('#app');
